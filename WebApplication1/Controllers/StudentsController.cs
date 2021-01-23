dffb9822e08eaf132731540f5fcd1a87d22213d1
TahaSavas <taha.savas@hotmail.com>
2021-01-23 22:17:05 +0100

    Add project files.

---
+++ WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{ 
+
+    [ApiController] 
+    [Route("api/students")] 
+    public class StudentsController : ControllerBase 
+    {
+
+        private IDbService _dbService; 
+        public StudentsController(IDbService dbService)
+        {
+            this._dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id) 
+        {
+            if (id == 1)
+            {
+                return Ok("Taha");
+
+            }else if (id == 2)
+            {
+                return Ok("Johny");
+            }
+            return NotFound("no student ");
+        }
+
+      
+        [HttpPost]   
+        public IActionResult createStudent(Student student) 
+        {                                                   
+
+            student.indexnumber = $"s{new Random().Next(1,20000)}";
+           
+            return Ok(student);
+        }
+
+       
+  
+
+        Student student = new Student();
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+        
+            return Ok("Update completed");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {     
+         
+            return Ok("Delete completed");
+        }
+
+
+    }
+}
---
+++ WebApplication1/Services/MockDbService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService() 
+        {
+            _students = new List<Student>
+            {
+                new Student{IdStudent=1 ,fname="Taha" ,lname="Savas" ,indexnumber="s18409"},
+                 new Student{IdStudent=2 ,fname="Johny " ,lname="Bravo" ,indexnumber="s01234"},
+                  new Student{IdStudent=3 ,fname="Elon" ,lname="Musk" ,indexnumber="s07711"}
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
